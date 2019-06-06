f6f4fed80462e56f3617d45c80a08de80bcac3dd
Dhiraj Basukala <dhiraj.basukala@gmail.com>
2019-06-06 10:33:56 +0545

    Added meta tags for the site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: `Dhiraj Basukala: Frontend developer from Nepal`,
+    siteUrl: `https://www.dhirajbasukala.com.np`,
+    description: `A passionate frontend developer from Nepal with a decade of experience in web industory.`,
+  },
   plugins: [`gatsby-plugin-sass`]
 }
