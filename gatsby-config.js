12f89e48cd01da884ede973398a9752fa217a570
Dhiraj Basukala <dhiraj.basukala@gmail.com>
2020-07-30 22:24:38 +0545

    Add offline and manifest pllugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,19 @@ module.exports = {
     siteUrl: `https://www.dhirajbasukala.com.np`,
     description: `A passionate frontend developer from Nepal with a decade of experience in web industory.`,
   },
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `basukala`,
+        short_name: `dhirajbasukala`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#e80858`,
+        display: `standalone`,
+      },
+    },
+  ],
 }
